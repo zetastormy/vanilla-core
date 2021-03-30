1c14aecaae740fa198d73094df2a3ee7d27be6f3
Cristóbal Veas <crisfeelspeed@gmail.com>
2021-03-30 16:42:32 -0300

    Remove unnecesary color replacement method

--- src/main/java/org/zafire/studios/vanillacore/util/MessageParser.java
+++ src/main/java/org/zafire/studios/vanillacore/util/MessageParser.java
@@ -1,30 +1,19 @@
 package org.zafire.studios.vanillacore.util;
 
 import org.bukkit.Bukkit;
-import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
 
 import me.clip.placeholderapi.PlaceholderAPI;
 
 public final class MessageParser {
-    
+
     public String parse(final String string, final Player player) {
         String stringReplacement;
 
-        if (PlaceholderAPI.containsPlaceholders(string)) {
-            Bukkit.getLogger().warning("Placeholders detected!");
+        stringReplacement = PlaceholderAPI.setPlaceholders(player, string);
 
-            stringReplacement = PlaceholderAPI.setPlaceholders(player, string);
-            
-            Bukkit.getLogger().warning("String with placeholders: " + stringReplacement);
-        }
-        
-        stringReplacement = replaceColors(string);
+        Bukkit.getLogger().warning("String with placeholders: " + stringReplacement);
 
         return stringReplacement;
-    }
-
-    private String replaceColors(final String string) {
-        return ChatColor.translateAlternateColorCodes('&', string);
     }
 }
