8d982965efeb11a000811fe3b6bb2db6a5648c6e
Cristóbal Veas <crisfeelspeed@gmail.com>
2021-03-29 22:32:51 -0300

    Add test case for teleportation

--- src/main/java/org/zafire/studios/vanillacore/VanillaCorePlugin.java
+++ src/main/java/org/zafire/studios/vanillacore/VanillaCorePlugin.java
@@ -1,10 +1,13 @@
 package org.zafire.studios.vanillacore;
 
+import java.io.File;
 import java.util.logging.Logger;
 
 import org.bukkit.Server;
+import org.bukkit.plugin.PluginDescriptionFile;
 import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.plugin.java.JavaPluginLoader;
 import org.zafire.studios.vanillacore.listener.PlayerJoinListener;
 import org.zafire.studios.vanillacore.util.LocationSelector;
 
@@ -13,6 +16,14 @@ public final class VanillaCorePlugin extends JavaPlugin {
     private Server server;
     private PluginManager pluginManager;
     private LocationSelector locationSelector;
+
+    public VanillaCorePlugin() {
+        super();
+    }
+
+    protected VanillaCorePlugin(final JavaPluginLoader loader, final PluginDescriptionFile description, final File dataFolder, final File file) {
+        super(loader, description, dataFolder, file);
+    }
 
     @Override
     public void onEnable() {
--- src/test/java/org/zafire/studios/vanillacore/VanillaCoreTest.java
+++ src/test/java/org/zafire/studios/vanillacore/VanillaCoreTest.java
@@ -1,13 +1,37 @@
 package org.zafire.studios.vanillacore;
 
-import static org.junit.Assert.assertEquals;
-
+import org.bukkit.Location;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
+import org.zafire.studios.vanillacore.util.LocationSelector;
+
+import be.seeseemelk.mockbukkit.MockBukkit;
+import be.seeseemelk.mockbukkit.ServerMock;
+import be.seeseemelk.mockbukkit.entity.PlayerMock;
 
 class VanillaCoreTest {
+    private ServerMock server;
+    private VanillaCorePlugin plugin;
+    private PlayerMock player;
+
+    @Before
+    public void setUp() {
+        server = MockBukkit.mock();
+        plugin = (VanillaCorePlugin) MockBukkit.load(VanillaCorePlugin.class);
+        player = server.addPlayer("ZetaStormy");
+    }
 
     @Test
-    void testApp() {
-        assertEquals(1, 1);
+    public void testLocation() {
+        LocationSelector locationSelector = plugin.getLocationSelector();
+        Location location = locationSelector.getRandomLocation();
+        player.teleport(location);
+        player.assertLocation(location, 10);
+    }
+
+    @After
+    public void tearDown() {
+        MockBukkit.unmock();
     }
 }
